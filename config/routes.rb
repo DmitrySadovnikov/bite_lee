3aaa82b16b5fb26359134eb995b5146a0e2fb269
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-08-25 15:00:22 +0300

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :links, only: %i[create]
+    end
+  end
+
+  get '*suffix', to: 'api/v1/links#handle_get'
+  post '*suffix', to: 'api/v1/links#handle_post'
 end
